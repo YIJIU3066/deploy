4171dee4b689371df5f0fc27d49ad6a853af1406
YIJIU3066 <yijiu3066@gmail.com>
2022-12-28 15:51:25 +0800

    deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,17 +27,23 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+if (process.env.NODE_ENV === "development") {
+	app.use(cors());
+}
+
+app.get("/api", (req,res) => {
+  console.log("GET / api");
+  res.send({ message: "Hello from the server!"}).status(200)
+})
+
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.use(express.static(path.join(__dirname, "../../frontend", "build")));
   app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../../rontend", "build", "index.html"));
   });
 }
 
 const port = process.env.PORT || 4000;
-if (process.env.NODE_ENV === "development") {
-	app.use(cors());
-}
 
