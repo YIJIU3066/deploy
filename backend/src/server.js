f2585890ce4f4329828782a1d9bb1da5e12ea848
YIJIU3066 <yijiu3066@gmail.com>
2022-12-28 16:35:06 +0800

    depl

--- backend/src/server.js
+++ backend/src/server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "../../frontend", "build")));
   app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "../../rontend", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "../../frontend", "build", "index.html"));
   });
 }
 
