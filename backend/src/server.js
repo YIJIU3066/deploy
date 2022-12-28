9ddb23767ff08aca31c904d859ca6ba76b4d7cd3
YIJIU3066 <yijiu3066@gmail.com>
2022-12-28 16:17:46 +0800

    deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -45,5 +45,9 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+
 const port = process.env.PORT || 4000;
+app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`),
+);
 
